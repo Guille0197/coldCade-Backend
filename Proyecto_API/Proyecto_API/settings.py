55956fbd376ff087cc20116ea0a4c1347e375921
Jorge Avila <jorgee.avilah@gmail.com>
2022-05-17 21:40:47 -0500

    add properties for deploy

--- Proyecto_API/Proyecto_API/settings.py
+++ Proyecto_API/Proyecto_API/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Proyecto_API.urls'
@@ -135,3 +137,14 @@ STATIC_URL = 'static/'
 #REST_FRAMEWORK = {
 #    #'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
 #}
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
